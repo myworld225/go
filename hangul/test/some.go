1d39c130129e0a00ceaa8dac97da2a536abe880c
Ks Park <myworld225@naver.com>
2016-12-28 12:21:16 +0900

    first commit

---
+++ hangul/test/some.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "github.com/myworld225/go/hangul"
+
+func main() {
+	fmt.Println("Hangul Test")
+	var a = hangul.HasConsonantSuffix("테스트")
+	var b = hangul.HasConsonantSuffix("입니깡")
+	fmt.Println(a, b)
+}
